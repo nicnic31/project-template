d94cd0714220bc7510081838b4a9d793247f7846
nicnic31 <permejonics@gmail.com>
2023-11-16 17:26:37 +0800

    added helperText as props on Input component

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -10,15 +10,18 @@ export interface IInputProps
   textSize?: TextSizeName;
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
+  helperText?: string;
 }
 
 const Input = forwardRef<HTMLInputElement, IInputProps>(
   (
     {
-      variant = "outlined",
+      variant = "filled",
       textSize = "medium",
       startIcon,
       endIcon,
+      helperText,
+      onChange,
       className,
       ...inputProps
     },
@@ -29,7 +32,7 @@ const Input = forwardRef<HTMLInputElement, IInputProps>(
         {startIcon && <div className="absolute top-4 left-4">{startIcon}</div>}
         <input
           className={cn(
-            "py-3 text-black border-gray focus:outline-0 focus:border-dark-gray w-full",
+            "py-3 text-black focus:outline-0 w-full",
             variant === "outlined"
               ? "rounded border focus:border-2"
               : "rounded-t border-b focus:border-b-2",
@@ -38,11 +41,20 @@ const Input = forwardRef<HTMLInputElement, IInputProps>(
             endIcon && "pl-3 pr-12",
             startIcon && "pl-12 pr-3",
             !endIcon && !startIcon && "px-3",
+            helperText
+              ? "border-dark-red focus:border-dark-red"
+              : " border-gray focus:border-dark-gray",
             className
           )}
-          {...inputProps}
+          onChange={onChange}
           ref={ref}
+          {...inputProps}
         />
+        {helperText && (
+          <p className="text-red text-xs tracking-wide font-medium">
+            {helperText}
+          </p>
+        )}
         {endIcon && <div className="absolute top-4 right-5">{endIcon}</div>}
       </div>
     );
